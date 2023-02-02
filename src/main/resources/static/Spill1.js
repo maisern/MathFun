98a37779c05e98009460c9e14bcde7f40c51c3aa
Maisern <mai8lise@hotmail.com>
2023-02-02 13:43:54 +0100

    Første utkast av første spill

--- src/main/resources/static/Spill1.js
+++ src/main/resources/static/Spill1.js
@@ -1,6 +1,21 @@
 
 function testfunk(){
-    document.getElementById('fra_js').innerHTML = 'fra script';
-    window.alert("5 + 6 != " + 5 + 6); // problem løst ;P
+    window.alert("5 + 6 != " + 5 + 6);
 }
 
+function startSpill(){
+    document.getElementById('spill').style.display='block';
+    document.getElementById('start').style.display='none';
+}
+
+function svarGitt(svar){
+    let tall = Number(svar);
+    if (tall===20){
+        document.write("Riktig")
+        document.write(tall);
+    }else {
+        document.write("Feil!")
+        document.write(tall);
+
+    }
+}
