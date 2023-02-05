d3fca73265dc59b4810ad2e3d3fd7a07de4c6372
Maisern <mai8lise@hotmail.com>
2023-02-05 18:42:15 +0100

    kan nå sjekke om svaret avgitt er riktig. start spillet-knappen kommer tilbake.

--- src/main/resources/static/Spill1.js
+++ src/main/resources/static/Spill1.js
@@ -1,27 +1,36 @@
 
-function testfunk(){
-    window.alert("5 + 6 != " + 5 + 6);
-}
+let t1, t2, t3, svar
 
 function startSpill(){
-    let t1, t2;
+
     t1 = Math.floor(Math.random()*10);
     t2 = Math.floor(Math.random()*10);
     document.getElementById('sporsmal').innerHTML = t1 + ' * ' + t2;
     document.getElementById('spill').style.display='block';
     document.getElementById('start').style.display='none';
-    return t1 * t2;
+    //document.getElementById('input').innerHTML = '';
+
+    t3 = t1 * t2
 }
 
-function svarGitt(svar, t3){
+function nesteSpill(){
+
+    t1 = Math.floor(Math.random()*10);
+    t2 = Math.floor(Math.random()*10);
+    document.getElementById('sporsmal').innerHTML = t1 + ' * ' + t2;
+    //document.getElementById('input').innerHTML = '';
+
+    t3 = t1 * t2
+}
+
+function svarGitt(svar){
     let tall = Number(svar);
     if (tall===t3){
-        alert("Riktig");
-        startSpill();
-    }else {
-        alert("Feil!");
-        startSpill();
-        document.write(t3)
+        document.getElementById('resultat').innerHTML='Riktig!'
 
+    }else {
+        document.getElementById('resultat').innerHTML='Feil!'
     }
+    //nesteSpill()
+
 }
